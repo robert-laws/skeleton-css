6fb10066e69642d2bb2b1f8e519e49f244741e0d
robert-laws <robaws@me.com>
2017-08-15 21:04:46 +0300

    forms and optimization sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var concatCSS = require('gulp-concat-css')
+var cleanCSS = require('gulp-clean-css')
+var uncss = require('gulp-uncss')
 
-gulp.task('serve', function() {  
+gulp.task('serve',['css'], function() {
   browserSync.init({
     server: "./"
   })
 
   gulp.watch(
-    ["./*.html","./css/*.css"]).on('change', browserSync.reload)
+    [
+      "./*.html",
+      "./css/*.css",
+      ['css']
+    ]).on('change', browserSync.reload)
+})
+
+gulp.task('css', () => {
+  return gulp.src(['./css/skeleton.css', './css/normalize.css', './css/main.css'])
+    .pipe(concatCSS('bundle.css'))
+    .pipe(uncss({html: ['index.html']}))
+    .pipe(cleanCSS({compatability: 'ie8'}))
+    .pipe(gulp.dest('css'))
 })
 
 gulp.task('default', ['serve'])
